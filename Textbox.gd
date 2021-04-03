3485b3ab86ac855f1f69103dc571c625026ca570
Jon Topielski <jontopielski@Jons-MacBook-Pro.local>
2021-04-02 19:20:05 -0700

    Add tutorial code

---
+++ Textbox.gd
@@ -0,0 +1,77 @@
+extends CanvasLayer
+
+const CHAR_READ_RATE = 0.05
+
+onready var textbox_container = $TextboxContainer
+onready var start_symbol = $TextboxContainer/MarginContainer/HBoxContainer/Start
+onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
+onready var label = $TextboxContainer/MarginContainer/HBoxContainer/Label
+
+enum State {
+	READY,
+	READING,
+	FINISHED
+}
+
+var current_state = State.READY
+var text_queue = []
+
+func _ready():
+	print("Starting state: State.READY")
+	hide_textbox()
+	queue_text("Excuse me wanderer where can I find the bathroom?")
+	queue_text("Why do we not look like the others?")
+	queue_text("Because we are free assets from opengameart!")
+	queue_text("Thanks for watching!")
+
+func _process(delta):
+	match current_state:
+		State.READY:
+			if !text_queue.empty():
+				display_text()
+		State.READING:
+			if Input.is_action_just_pressed("ui_accept"):
+				label.percent_visible = 1.0
+				$Tween.stop_all()
+				end_symbol.text = "v"
+				change_state(State.FINISHED)
+		State.FINISHED:
+			if Input.is_action_just_pressed("ui_accept"):
+				change_state(State.READY)
+				hide_textbox()
+
+func queue_text(next_text):
+	text_queue.push_back(next_text)
+
+func hide_textbox():
+	start_symbol.text = ""
+	end_symbol.text = ""
+	label.text = ""
+	textbox_container.hide()
+
+func show_textbox():
+	start_symbol.text = "*"
+	textbox_container.show()
+
+func display_text():
+	var next_text = text_queue.pop_front()
+	label.text = next_text
+	label.percent_visible = 0.0
+	change_state(State.READING)
+	show_textbox()
+	$Tween.interpolate_property(label, "percent_visible", 0.0, 1.0, len(next_text) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+func change_state(next_state):
+	current_state = next_state
+	match current_state:
+		State.READY:
+			print("Changing state to: State.READY")
+		State.READING:
+			print("Changing state to: State.READING")
+		State.FINISHED:
+			print("Changing state to: State.FINISHED")
+
+func _on_Tween_tween_completed(object, key):
+	end_symbol.text = "v"
+	change_state(State.FINISHED)
